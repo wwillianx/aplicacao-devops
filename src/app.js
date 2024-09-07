dd447226db8a6c0355fccd3d335ad5a929ba3fc1
Willian Scheuermann <williamscheuermann10@gmail.com>
2024-09-06 23:15:28 -0300

    integrantes adicionados

--- src/app.js
+++ src/app.js
@@ -3,9 +3,21 @@ const app = express();
 const port = 3005;
 
 app.listen(port, () => {
-    console.log("App listening on port: " + port);
-  });
+  console.log("App listening on port: " + port);
+});
 
 app.get('/', (req, res) => {
-    res.sendFile('views/index.html', { root: __dirname })
-    })
+  res.sendFile('views/index.html', { root: __dirname })
+})
+
+data = {
+  'integrantes': [
+    { 'nome': 'Riskala Tedoldi' },
+    { 'nome': 'Leonardo Peron' },
+    { 'nome': 'Willian Scheuermann' }
+  ]
+}
+
+app.get('/integrantes', (req, res) => {
+  res.json(data)
+})
